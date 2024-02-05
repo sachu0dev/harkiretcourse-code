ceae49713918035f82aade5209f9334df203f6bd
sachu0dev <sushil.dev.in@gmail.com>
2024-02-05 18:32:43 +0530

    promises learned

--- week-2/week2.1.js
+++ week-2/week2.1.js
@@ -19,19 +19,52 @@
 
 
 // async functions
-const fs = require('fs');
-let a = "first console";
-console.log(a);
+// const fs = require('fs');
+// let a = "first console";
+// console.log(a);
 
-fs.readFile("a.txt", "utf8", (err, data)=>{
-  console.log("this is read from file: " + data);
-});
-let ans = 0;
-for(let i=0; i< 100; i++){
-  ans += i;
-}
-console.log(ans);
+// fs.readFile("a.txt", "utf8", (err, data)=>{
+//   console.log("this is read from file: " + data);
+// });
+// let ans = 0;
+// for(let i=0; i< 100; i++){
+//   ans += i;
+// }
+// console.log(ans);
 
+
+// console.log("hie");
+
+// setTimeout(()=>{
+//   console.log("hi there from inside");
+// }, 5000);
+
+// let a = 0;
+// for(let i = 0; i < 10000; i++){
+//   a += i;
+// }
+// console.log(a);
+
+// const fs = require("fs");
+// function putNewText(){
+//   fs.readFile("a.txt", "utf8", (err, data)=>{
+//     data = data + "new text added";
+//     fs.writeFile("a.txt", data, ()=>{
+//       console.log("done");
+//     })
+//   });
+// }
+
+// putNewText();
+// function myOwnSetTimeout(cb,time){
+//   setTimeout(cb, time);
+// }
+
+// myOwnSetTimeout(()=>{
+//   console.log("hi there");
+// }, 1000);
+
+// 
 
 
 
