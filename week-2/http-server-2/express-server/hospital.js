1eebe974eba16df4a5716e095fcfac9a554364e5
sachu0dev <sushil.dev.in@gmail.com>
2024-02-10 10:22:00 +0530

    hospital backed server

--- week-2/http-server-2/express-server/hospital.js
+++ week-2/http-server-2/express-server/hospital.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const app = express();
-
 const users = [{
   name: 'abhi',
   kidneys: [{
     healthy: false
-  },
-  {
-    healthy : true
   }]
 }];
 
+app.use(express.json());
 app.get('/',  (req, res)=>{
   const userKidneys = users[0].kidneys;
   const numberofKidneys = userKidneys.length;
@@ -29,7 +26,63 @@ app.get('/',  (req, res)=>{
 })
 
 app.post("/", (req, res)=>{
+  const isHealthy = req.body.isHealthy;
+  users[0].kidneys.push({
+    healthy: isHealthy
+  })
+  res.json({
+    msg: "Done!"
+  })
+})
+
+app.put('/', (req,res)=>{
+  // 411 status
+  let atleastonekidneyishealthy = isThereUnhealthyKidney();
+  if(atleastonekidneyishealthy){
+    for(let i = 0; i<users[0].kidneys.length; i++){
+      users[0].kidneys[i].healthy = req.body.isHealthy;
+    }
+    res.json({
+      "message": "Done!"
+    })
+  }else{
+    res.status(411).send({
+      msg : "No unhealthy Kidney"
+    });
+  }
+})
+
+app.delete('/', (req, res)=>{
+  // you should return a 411 status
+  // only if atleast one unhealthy kidney is there do this , else return 411
+  let atleastonekidneyishealthy = isThereUnhealthyKidney();
+  if(atleastonekidneyishealthy){
+    const newkidneys = [];
+    for(let i = 0; i<users[0].kidneys.length; i++){
+      if(users[0].kidneys[i].healthy){
+        newkidneys.push(users[0].kidneys[i]);
+      }
+    }
+    users[0].kidneys = newkidneys;
+    res.json({
+      "message": "Done!"
+    })
+  } else {
+    res.status(411).send({
+      msg : "No unhealthy Kidney"
+    });
+  }
   
 })
+
+function isThereUnhealthyKidney(){
+  let atleastonekidneyishealthy = false;
+  for(let i = 0; i<users[0].kidneys.length; i++){
+    if(!users[0].kidneys[i].healthy){
+      atleastonekidneyishealthy = true;
+    }
+  }
+  return atleastonekidneyishealthy;
+}
 
 app.listen(3001);
