e2c0e4970487c502613511d9839e1741f65c5135
sachu0dev <sushil.dev.in@gmail.com>
2024-02-21 18:41:21 +0530

    middleware

--- week-3/3.1/middleware.js
+++ week-3/3.1/middleware.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const app = express();
 
-app.get('/health-checkup',(req, res)=>{
+function userMiddleware(req, res, next){
   const username = req.headers.username;
   const password = req.headers.password;
-  const kidneyId = req.query.kidneyId;
   if(username !== "admin" || password !== "admin123"){
     return res.status(400).json({msg:'auth fail'});
-    
-  } 
-
-if(kidneyId != 1 && kidneyId != 2){
-    return res.status(401).json({msg:'something up with your kidneys'});
+  } else {
+    next();
   }
+}
+function kidenyMiddleWare(req,res,next){
+  const kidneyId = req.query.kidneyId;
+if(kidneyId != 1 && kidneyId != 2){
+    return res.status(401).json({msg:'somethings up with your kidneys'});
+  } else {
+    next();
+  }
+}
+
+app.get('/health-checkup',userMiddleware,kidenyMiddleWare,(req, res)=>{
   res.json({
     msg: "your kidenys are healthy"
   })
 })
+
+
 app.listen(3000);
-// dumb way of writng user validation
-// violates dry rule 
-// have to write logic in every route
