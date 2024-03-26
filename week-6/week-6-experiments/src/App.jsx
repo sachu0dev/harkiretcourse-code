0025c751e69d320b31d827af50c1f71a285727cc
sachu0dev <sushil.dev.in@gmail.com>
2024-03-26 19:22:02 +0530

    week-6

--- week-6/week-6-experiments/src/App.jsx
+++ week-6/week-6-experiments/src/App.jsx
@@ -1,51 +1,69 @@
 import { useState } from "react";
-
+var tempId = 4;
 function App() {
   const [todos, setTodos] = useState([
     {
+      id: 1,
       title: "go to gym",
       description: "this is description",
     },
     {
+      id: 2,
       title: "go to gym",
       description: "this is description",
     },
     {
+      id: 3,
       title: "go to gym",
       description: "this is description",
     },
   ]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  function addTodo() {
+    const newTodos = [
+      ...todos,
+      {
+        id: tempId,
+        title,
+        description,
+      },
+    ];
+    setTodos(newTodos);
+    tempId = tempId + 1;
+  }
   return (
     <>
-      <Input />
-      <button>Add Todo</button>
+      <div>
+        <input
+          type="text"
+          placeholder="title"
+          onChange={(e) => {
+            const value = e.target.value;
+            setTitle(value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="title"
+          onChange={(e) => {
+            const value = e.target.value;
+            setDescription(value);
+          }}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
       {todos.map((todo) => {
         return (
-          <div>
+          <div key={todo.id}>
             <h3>{todo.title}</h3>
             <h3>{todo.description}</h3>
+            <p>{todo.id}</p>
           </div>
         );
       })}
     </>
   );
 }
-
-const Todo = ({ title, description }) => {
-  return (
-    <div>
-      <h3>{title}</h3>
-      <h3>{description}</h3>
-    </div>
-  );
-};
-const Input = () => {
-  return (
-    <div>
-      <input type="text" placeholder="title" />
-      <input type="text" placeholder="title" />
-    </div>
-  );
-};
 
 export default App;
