ed8853c1c4ac2f104b144a92fe37ee4194f42f10
sachu0dev <sushil.dev.in@gmail.com>
2024-03-26 16:27:53 +0530

    used memo

--- week-6/week-6-experiments/src/App.jsx
+++ week-6/week-6-experiments/src/App.jsx
@@ -1,11 +1,30 @@
-import { Header, HeaderWithBtn } from "./componets/Header";
+import { memo, useState } from "react";
 function App() {
+  const [randomNo, setRandomNo] = useState("sushil");
+  function handelClick() {
+    const no = Math.random();
+    setRandomNo(no);
+  }
+
   return (
     <>
-      <HeaderWithBtn />
-      <Header title={"second header"} />
+      <button onClick={handelClick}>Click to change</button>
+      <Header title={randomNo} />
+      <Header title="second title" />
+      <Header title="second title" />
+      <Header title="second title" />
+      <Header title="second title" />
+      <Header title="second title" />
     </>
   );
 }
+const Header = memo(function Header({ title }) {
+  console.log(title);
+  return (
+    <div>
+      <h1>this is title : {title}</h1>
+    </div>
+  );
+});
 
 export default App;
